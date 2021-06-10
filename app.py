979e7de3ab67527558c53640a31a0c6a80e8b674
Yeon <tmddusgood@gmail.com>
2021-06-11 00:19:50 +0900

    chapter1 finished

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 import jwt
 import datetime
@@ -13,7 +15,8 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 SECRET_KEY = 'SPARTA'
 
 client = MongoClient('3.133.93.99', 27017, username="test", password="test")
-db = client.dbsparta_plus_week4
+db = client.netoon
+
 
 
 @app.route('/')
@@ -22,11 +25,66 @@ def home():
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         user_info = db.users.find_one({"username": payload["id"]})
-        return render_template('index.html', user_info=user_info)
-    except jwt.ExpiredSignatureError:
-        # msg = 'expired'
-        return redirect(url_for("login"))
+        title_list = list(db.webtoon.find({},{'_id':False}))
 
+        # 랭킹정하기
+        raw_db = list(db.posts.find({}, {'title': 1, 'comment': 1, 'img_url': 1}))
+        print(raw_db)
+
+        # 순위랑 title 리스트
+        list_toon = []  # title만 따로 넣을 리스트 만들기
+        for i in range(len(raw_db)):
+            a = raw_db[i]['title']
+            list_toon.append(a)
+
+        print(list_toon)
+        count = {}
+        for j in list_toon:
+            try:
+                count[j] += 1
+            except:
+                count[j] = 1
+
+        # count = {'웹툰파일이름[0]': 포스팅한수, '웹툰파일이름[0]': 1, 'apple': 1}
+        # value값에 맞게 내림차순으로 정렬
+        new_count = sorted(count.items(), reverse=True, key=lambda item: item[1])
+        print(new_count)
+        give_information = []
+        for i in new_count:
+            give_information.append(list(i))
+
+        print(give_information)
+        top10_list = []
+        for i in give_information:
+            top10_list.append(i[0])
+            print(i[0], i[1])
+
+        comment_dict = {}
+        for title in top10_list:
+            comments = []
+            for item in raw_db:
+                if item['title'] == title:
+                    comments.append(item['comment'])
+                comment_dict[title] = comments
+
+        for i in give_information:
+            var1 = comment_dict[i[0]]
+            rand_comment = var1[random.randint(0, len(var1) - 1)]
+            i.append(rand_comment)
+
+        img_dict = {}
+        for title in top10_list:
+            for item in raw_db:
+                if item['title'] == title:
+                    img_dict[title] = item['img_url']
+
+        for i in give_information:
+            img_url = img_dict[i[0]]
+            i.append(img_url)
+
+        return render_template('index.html', user_info=user_info, title_list=title_list, rank=give_information[:9])
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
@@ -35,21 +93,6 @@ def home():
 def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
-
-
-# 우리는 쓰지 않을 함수
-@app.route('/user/<username>')
-def user(username):
-    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
-
-        user_info = db.users.find_one({"username": username}, {"_id": False})
-        return render_template('user.html', user_info=user_info, status=status)
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
 
 
 @app.route('/sign_in', methods=['POST'])
@@ -64,8 +107,8 @@ def sign_in():
     if result is not None:
         payload = {
             'id': username_receive,
-            # 'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
-            'exp': datetime.utcnow() + timedelta(seconds=5)  # 로그인 5초 유지
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+            # 'exp': datetime.utcnow() + timedelta(seconds=5)  # 로그인 5초 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
 
@@ -81,16 +124,15 @@ def sign_up():
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        "username": username_receive,
-        "password": password_hash,
-        "profile_name": username_receive,
-        "profile_pic": "",
-        "profile_pic_real": "profile_pics/profile_placeholder.png",
-          "profile_info": ""
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "profile_name": username_receive,  # 프로필 이름 기본값은 아이디
+        "profile_pic": "",  # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png",  # 프로필 사진 기본 이미지
+        "profile_info": ""  # 프로필 한 마디
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
-
 
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
@@ -100,47 +142,26 @@ def check_dup():
     return jsonify({'result': 'success', 'exists': exists})
 
 
-@app.route('/update_profile', methods=['POST'])
-def save_img():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        username = payload["id"]
-        name_receive = request.form["name_give"]
-        about_receive = request.form["about_give"]
-        new_doc = {
-            "profile_name": name_receive,
-            "profile_info": about_receive
-        }
-        if 'file_give' in request.files:
-            file = request.files["file_give"]
-            filename = secure_filename(file.filename)
-            extension = filename.split(".")[-1]
-            file_path = f"profile_pics/{username}.{extension}"
-            file.save("./static/" + file_path)
-            new_doc["profile_pic"] = filename
-            new_doc["profile_pic_real"] = file_path
-        db.users.update_one({'username': payload['id']}, {'$set': new_doc})
-        return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
-
 
 @app.route('/posting', methods=['POST'])
 def posting():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        # 포스팅하기
         user_info = db.users.find_one({"username": payload["id"]})
+        title_receive = request.form["title_give"]
         comment_receive = request.form["comment_give"]
         date_receive = request.form["date_give"]
-        print(type(date_receive))
+
+        toon_img = db.webtoon.find_one({"title": title_receive})
+
         doc = {
             "username": user_info["username"],
             "profile_name": user_info["profile_name"],
             "profile_pic_real": user_info["profile_pic_real"],
+            "title": title_receive,
             "comment": comment_receive,
+            "img_url": toon_img['img'],
             "date": date_receive
         }
         db.posts.insert_one(doc)
@@ -154,29 +175,12 @@ def get_posts():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        my_username = payload["id"]
-        username_receive = request.args.get("username_give")
-        if username_receive == "":
-            posts = list(db.posts.find({}).sort("date", -1).limit(20))
-        else:
-            posts = list(db.posts.find({"username": username_receive}).sort("date", -1).limit(20))
-
+        posts = list(db.posts.find({}).sort("date", -1).limit(20))
         for post in posts:
             post["_id"] = str(post["_id"])
-
             post["count_heart"] = db.likes.count_documents({"post_id": post["_id"], "type": "heart"})
-            post["heart_by_me"] = bool(
-                db.likes.find_one({"post_id": post["_id"], "type": "heart", "username": my_username}))
-
-            post["count_star"] = db.likes.count_documents({"post_id": post["_id"], "type": "star"})
-            post["star_by_me"] = bool(
-                db.likes.find_one({"post_id": post["_id"], "type": "star", "username": my_username}))
-
-            post["count_like"] = db.likes.count_documents({"post_id": post["_id"], "type": "like"})
-            post["like_by_me"] = bool(
-                db.likes.find_one({"post_id": post["_id"], "type": "like", "username": my_username}))
-
-        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "posts": posts})
+            post["heart_by_me"] = bool(db.likes.find_one({"post_id": post["_id"], "type": "heart", "username": payload['id']}))
+        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "posts":posts})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
@@ -186,7 +190,6 @@ def update_like():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        # 좋아요 수 변경
         user_info = db.users.find_one({"username": payload["id"]})
         post_id_receive = request.form["post_id_give"]
         type_receive = request.form["type_give"]
@@ -201,10 +204,19 @@ def update_like():
         else:
             db.likes.delete_one(doc)
         count = db.likes.count_documents({"post_id": post_id_receive, "type": type_receive})
-        print(count)
         return jsonify({"result": "success", 'msg': 'updated', "count": count})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
+
+
+
+@app.route('/delete_post', methods=['POST'])
+def delete_post():
+    _id = request.form['id_give']
+    db.posts.delete_one({'_id': ObjectId(_id)})
+
+    return jsonify({'result': 'success'})
+
 
 
 if __name__ == '__main__':
--- static/myjs.js
+++ static/myjs.js
@@ -36,7 +36,7 @@ function get_posts(username) {
                     let class_star = post['star_by_me'] ? "fa-star" : "fa-star-o"
                     let class_like = post['like_by_me'] ? "fa-thumbs-up" : "fa-thumbs-o-up"
 
-                    let html_temp = `<div class="box" id="${post["_id"]}">
+                    let html_temp = `<div class="post" id="${post["_id"]}">
                                         <article class="media">
                                             <div class="media-left">
                                                 <a class="image is-64x64" href="/user/${post['username']}">
@@ -148,4 +148,223 @@ function toggle_like(post_id, type) {
         })
 
     }
+}
+
+
+function toggle_like(post_id, type) {
+    console.log(post_id, type)
+    let $a_like = $(`#${post_id} a[aria-label='heart']`)
+    let $i_like = $a_like.find("i")
+    if ($i_like.hasClass("fa-heart")) {
+        $.ajax({
+            type: "POST",
+            url: "/update_like",
+            data: {
+                post_id_give: post_id,
+                type_give: type,
+                action_give: "unlike"
+            },
+            success: function (response) {
+                console.log("unlike")
+                $i_like.addClass("fa-heart-o").removeClass("fa-heart")
+                $a_like.find("span.like-num").text(response["count"])
+            }
+        })
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "/update_like",
+            data: {
+                post_id_give: post_id,
+                type_give: type,
+                action_give: "like"
+            },
+            success: function (response) {
+                console.log("like")
+                $i_like.addClass("fa-heart").removeClass("fa-heart-o")
+                $a_like.find("span.like-num").text(response["count"])
+            }
+        })
+
+    }
+}
+
+//코멘트 등록
+function post() {
+    let title = $('#selectarea-post').val()
+    let comment = $("#textarea-post").val()
+    let today = new Date().toISOString()
+    $.ajax({
+        type: "POST",
+        url: "/posting",
+        data: {
+            title_give: title,
+            comment_give: comment,
+            date_give: today
+        },
+        success: function (response) {
+            $("#modal-post").removeClass("is-active")
+            window.location.reload()
+        }
+    })
+}
+
+function time2str(date) {
+    let today = new Date()
+    let time = (today - date) / 1000 / 60  // 분
+
+    if (time < 60) {
+        return parseInt(time) + "분 전"
+    }
+    time = time / 60  // 시간
+    if (time < 24) {
+        return parseInt(time) + "시간 전"
+    }
+    time = time / 24
+    if (time < 7) {
+        return parseInt(time) + "일 전"
+    }
+    return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`
+}
+
+
+// 코멘트 보여주기
+function get_posts() {
+    $("#post-box").empty()
+    $.ajax({
+        type: "GET",
+        url: "/get_posts",
+        data: {},
+        success: function (response) {
+            if (response["result"] == "success") {
+                let posts = response["posts"]
+                for (let i = 0; i < posts.length; i++) {
+                    let post = posts[i]
+                    let time_post = new Date(post["date"])
+                    let time_before = time2str(time_post)
+                    let class_heart = post['heart_by_me'] ? "fa-heart" : "fa-heart-o"
+                    let count_heart = post['count_heart']
+                    let html_temp = `<div class="cardpost " id="${post["_id"]}" name="test_name">
+                                        <article class="media">
+                                            <div class="media-left">
+                                                <p class="image is-64x64"">
+                                                    <img class="is-rounded" src="${post['img_url']}"
+                                                         alt="Image">
+                                                </p>
+                                            </div>
+                                            <div class="media-content">
+                                                <div class="content">
+                                                    <p id="user_name">
+                                                        <strong>${post['title']}</strong> <small>@${post['username']}</small> <small>${time_before}</small>
+                                                        <br id='post_comment'>
+                                                        ${post['comment']}
+                                                    </p>
+                                                </div>
+                                                <nav class="level is-mobile">
+                                                    <div class="level-left">
+                                                        <a class="level-item is-sparta" aria-label="heart" onclick="toggle_like('${post['_id']}', 'heart')">
+                                                            <span class="icon is-small">
+                                                                <i class="fa ${class_heart}" aria-hidden="true"></i>
+                                                            </span>&nbsp;<span class="like-num">${count_heart}</span>
+                                                        </a>
+                                                    </div>
+                                                    <div>
+<!--                                                        <button type="button" class="btn btn-primary" data-toggle="modal"-->
+<!--                                                                data-target="#exampleModal"-->
+<!--                                                                data-whatever="@mdo">수정-->
+<!--                                                        </button>-->
+<!--                                                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"-->
+<!--                                                             aria-labelledby="exampleModalLabel" aria-hidden="true">-->
+<!--                                                            <div class="modal-dialog" role="document">-->
+<!--                                                                <div class="modal-content">-->
+<!--                                                                    <div class="modal-header">-->
+<!--                                                                        <h5 class="modal-title" id="exampleModalLabel">수정하기</h5>-->
+<!--                                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">-->
+<!--                                                                            <span aria-hidden="true">&times;</span>-->
+<!--                                                                        </button>-->
+<!--                                                                    </div>-->
+<!--                                                                    <div class="field">-->
+<!--                                                                        <p class="select is-rounded">-->
+<!--                                                                            <select id="modify-selectarea-post">-->
+<!--                                                                                {% for title in title_list %}-->
+<!--                                                                                    <option>{{ title['title'] }}</option>-->
+<!--                                                                                {% endfor %}-->
+<!--                                                                            </select>-->
+<!--                                                                        </p>-->
+<!--                                                                        <p class="control">-->
+<!--                                                                            <textarea id="modify-textarea-post" class="textarea"-->
+<!--                                                                                      placeholder="웹툰에 대해 작성해주세요."></textarea>-->
+<!--                                                                        </p>-->
+<!--                                                                    </div>-->
+<!--                                                                    <div class="modal-footer">-->
+<!--                                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">취소-->
+<!--                                                                        </button>-->
+<!--                                                                        <button type="button" class="btn btn-primary" onclick="update_post()">수정하기</button>-->
+<!--                                                                    </div>-->
+<!--                                                                </div>-->
+<!--                                                            </div>-->
+<!--                                                        </div>-->
+                                                        <button type="button" class="btn btn-primary" data-toggle="modal"
+                                                                data-target="#exampleModalCenter">
+                                                            삭제
+                                                        </button>
+                                                        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
+                                                             aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                                                <div class="modal-content">
+                                                                    <div class="modal-header">
+                                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                            <span aria-hidden="true">&times;</span>
+                                                                        </button>
+                                                                    </div>
+                                                                    <div class="modal-body">
+                                                                        정말로 삭제하시겠습니까?
+                                                                    </div>
+                                                                    <div class="modal-footer">
+                                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">취소
+                                                                        </button>
+                                                                        <button type="button" class="btn btn-primary" onclick="delete_post()">삭제하기</button>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </nav>
+                                            </div>
+                                        </article>
+                                    </div>`
+                    $("#post-box").append(html_temp)
+                }
+            }
+        }
+    })
+}
+
+
+$(document).ready(function () {
+    get_posts()
+})
+
+
+function delete_post() {
+    let var1 = document.getElementsByClassName('cardpost')
+    let id = var1[1]['id']
+    console.log(id)
+    $.ajax({
+        type: "POST",
+        url: "/delete_post",
+        data: {id_give: id},
+        success: function (response) {
+            if (response["result"] == "success") {
+                alert(response['result'])
+            }
+            window.location.reload()
+        }
+    })
+}
+
+function sign_out() {
+    $.removeCookie('mytoken', {path: '/'});
+    alert('로그아웃')
+    window.location.href = '/login'
 }
