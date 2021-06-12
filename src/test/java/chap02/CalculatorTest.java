b6471eef4b8b7ce83fe638b1c3faad6fe00baaa0
tmddusgood <tmddusgood@gmail.com>
2021-06-12 11:19:11 +0900

    test: CalculatorTest

---
+++ src/test/java/chap02/CalculatorTest.java
@@ -0,0 +1,16 @@
+package chap02;
+
+import chap02.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    void plus() {
+        int result = Calculator.plus(1, 2);
+        assertEquals(3, result);
+        assertEquals(5, Calculator.plus(4, 1));
+    }
+}
