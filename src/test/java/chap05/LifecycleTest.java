cb7a51c2f9f8f224c69731936a30fa6b53730a18
tmddusgood <tmddusgood@gmail.com>
2021-09-11 15:13:23 +0900

    test: AuthService

---
+++ src/test/java/chap05/LifecycleTest.java
@@ -0,0 +1,39 @@
+package chap05;
+
+import org.junit.jupiter.api.*;
+
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
+public class LifecycleTest {
+    /** 실행 순서를 알아보는 테스트
+     * 1. 테스트 메서드를 포함한 객체 생성
+     * 2. (존재하면) @BeforeEach 애노테이션이 붙은 메서드 실행
+     * 3. @Test 애노테이션이 붙은 메서드 실행
+     * 4. (존재하면0 @AfterEach 애노테이션이 붙은 메서드 실행
+     */
+
+    public LifecycleTest() {
+        System.out.println("new LifecycleTest");
+    }
+
+    @AfterEach
+    void tearDown(){
+        System.out.println("tearDown");
+    }
+
+    @BeforeEach
+    void setUp(){
+    System.out.println("setUp");
+    }
+
+    @Test
+    void a(){
+        System.out.println('A');
+    }
+
+    @Test
+    void b(){
+        System.out.println("B");
+    }
+}
