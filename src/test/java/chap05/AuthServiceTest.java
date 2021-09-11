7cb1179bfb48f98cc2bee3326287c3220c683105
tmddusgood <tmddusgood@gmail.com>
2021-09-11 10:24:21 +0900

    test: AuthService

--- src/test/java/chap05/AuthServiceTest.java
+++ src/test/java/chap05/AuthServiceTest.java
@@ -1,2 +1,21 @@
-package chap05;public class AuthServiceTest {
+package chap05;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AuthServiceTest {
+
+    @Test
+    void assertThrowTest() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class,
+                () -> {
+                    AuthService authService = new AuthService();
+                    authService.authenticate(null, null);
+                }
+        );
+        assertTrue(thrown.getMessage().contains("id"));
+    }
 }
