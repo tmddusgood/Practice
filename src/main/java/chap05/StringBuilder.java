928080031d27b7e69ad782b54a3215fe13dda5f0
tmddusgood <tmddusgood@gmail.com>
2021-09-11 16:29:21 +0900

    test: BadTest

--- src/main/java/chap05/StringBuilder.java
+++ src/main/java/chap05/StringBuilder.java
@@ -1,10 +1,10 @@
 package chap05;
 
-import java.io.File;
-import java.io.IOException;
+import java.math.*;
+import java.util.Random;
 
 public class StringBuilder {
-    public String buildString(){
-        return "hello world";
+    public String buildString(String str){
+        return str;
     }
 }
--- src/test/java/chap05/BadTest.java
+++ src/test/java/chap05/BadTest.java
@@ -1,5 +1,6 @@
 package chap05;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -7,14 +8,26 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/** 일부러 스태틱 변수를 인스턴스간 참조하게 만들어서 순서가 항상 맞는지 보기
+ *
+ */
 public class BadTest {
     private StringBuilder strBuilder = new StringBuilder();
     private static String str;
 
-    @Test
-    void fileCreateionTest() throws IOException{
-        String buildString = strBuilder.buildString();
+    /** @BeforeEach 가 아니라 @Test면 실패한다. 독립적으로 테스트 메서드 작성하라.
+     *
+     */
+    @BeforeEach
+    void stringBuildingTest(){
+        String buildString = strBuilder.buildString("hello");
         assertTrue(buildString.length() > 0);
         this.str = buildString;
+    }
+
+    @Test
+    void stringBuildingTest2(){
+        int data = this.str.length();
+        assertTrue(data > 0);
     }
 }
