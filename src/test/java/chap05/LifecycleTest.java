2f0cadd09827fde216245f98f8537b71460f194d
tmddusgood <tmddusgood@gmail.com>
2021-09-11 15:19:23 +0900

    test: AuthService

--- src/test/java/chap05/LifecycleTest.java
+++ src/test/java/chap05/LifecycleTest.java
@@ -7,7 +7,7 @@ import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 
 public class LifecycleTest {
     /** 실행 순서를 알아보는 테스트
-     * 1. 테스트 메서드를 포함한 객체 생성
+     * 1. 테스트 메서드를 포함한(정확히는 @Test 애노테이션이 붙은) 객체 생성
      * 2. (존재하면) @BeforeEach 애노테이션이 붙은 메서드 실행
      * 3. @Test 애노테이션이 붙은 메서드 실행
      * 4. (존재하면0 @AfterEach 애노테이션이 붙은 메서드 실행
