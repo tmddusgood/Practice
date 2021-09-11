d60c57bb31d38379cb95f32574e3b65168005873
tmddusgood <tmddusgood@gmail.com>
2021-09-11 11:46:21 +0900

    test: AuthService

--- src/test/java/chap05/AuthServiceTest.java
+++ src/test/java/chap05/AuthServiceTest.java
@@ -9,6 +9,15 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AuthServiceTest {
 
     @Test
+    void assertLocalDate(){
+        LocalDate dateTime1 = LocalDate.now();
+        LocalDate dateTime2 = LocalDate.now();
+
+        assertSame(dateTime1, dateTime2); // 같은 오브젝트인지 검증
+        assertEquals(dateTime1, dateTime2); // 같은 값인지 검증
+    }
+
+    @Test
     void assertThrowTest() {
         IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class,
                 () -> {
