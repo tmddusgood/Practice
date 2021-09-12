0afba044438aa5d699297fea94cfc831a3129b1d
tmddusgood <tmddusgood@gmail.com>
2021-09-12 10:50:01 +0900

    test: BadTest

--- src/test/java/chap05/BadTest.java
+++ src/test/java/chap05/BadTest.java
@@ -1,6 +1,8 @@
 package chap05;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -11,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /** 일부러 스태틱 변수를 인스턴스간 참조하게 만들어서 순서가 항상 맞는지 보기
  *
  */
+@DisplayName("@DisplayName, 메서드 간 독립성 확보 학습")
 public class BadTest {
     private StringBuilder strBuilder = new StringBuilder();
     private static String str;
@@ -25,6 +28,7 @@ public class BadTest {
         this.str = buildString;
     }
 
+    @DisplayName("String의 실제 값이 0보다 큰지 확인하는 메서드")
     @Test
     void stringBuildingTest2(){
         int data = this.str.length();
