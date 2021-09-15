46132ee34adca3a992ef1c2ab254d3df883e4640
tmddusgood <tmddusgood@gmail.com>
2021-09-15 20:24:24 +0900

    feat: Add Static Field Variable Game & @BeforeAll

--- src/test/java/chap06/BaseballGameTest.java
+++ src/test/java/chap06/BaseballGameTest.java
@@ -1,23 +1,28 @@
 package chap06;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+@DisplayName("야구 게임에 대한 테스트 메서드")
 public class BaseballGameTest {
-    @Test
-    void allMatch() {
-        BaseballGame game = new BaseballGame("456");
+    public static BaseballGame game;
+
+    @BeforeAll
+    static void givenGame() {
+        game = new BaseballGame("456");
     }
 
-
+    @DisplayName("456을 세팅하고 256에 대한 strike와 ball을 검증하는 테스트 메서드")
     @Test
     void exactMatch(){
-        // 정답이 456인 상황
-        BaseballGame game = new BaseballGame("456");
         // 실행
         Score score = game.guess("256");
         assertEquals(2, score.strikes);
+        assertEquals(1, score.balls);
     }
 }
