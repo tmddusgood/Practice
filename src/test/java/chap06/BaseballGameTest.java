14fd422746cdc7fb80f5a956d958a964aca471c5
tmddusgood <tmddusgood@gmail.com>
2021-09-15 19:39:21 +0900

    feat: BaseballGameTest

---
+++ src/test/java/chap06/BaseballGameTest.java
@@ -0,0 +1,10 @@
+package chap06;
+
+import org.junit.jupiter.api.Test;
+
+public class BaseballGameTest {
+    @Test
+    void allMatch() {
+        BaseballGame game = new BaseballGame("456");
+    }
+}
