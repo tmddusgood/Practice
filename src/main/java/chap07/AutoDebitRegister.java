bf91835fcc75a86386560a902dd2a20dc1ddce2c
tmddusgood <tmddusgood@gmail.com>
2021-09-16 11:29:25 +0900

    feat: Add AutoDebitRegister

--- src/main/java/chap07/AutoDebitRegister.java
+++ src/main/java/chap07/AutoDebitRegister.java
@@ -10,4 +10,19 @@ public class AutoDebitRegister {
         this.validator = validator;
         this.repository = repository;
     }
+
+    public RegisterResult register(AutoDebitReq req) {
+        CardValidity validity = validator.validate(req.getCardNumber());
+        if (validity != CardValidity.VALID) {
+            return RegisterResult.error(validity);
+        }
+        AutoDebitInfo info = repository.findOne(req.getUserId());
+        if (info != null) {
+            info.changeCardNumber(req.getCardNumber());
+        } else {
+            AutoDebitInfo newInfo = new AutoDebitInfo(req.getUserId(), req.getCardNumber(), LocalDateTime.now());
+            repository.save(newInfo);
+        }
+        return RegisterResult.success();
+    }
 }
